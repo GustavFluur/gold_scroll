ccc5994ffc3e4476d1563dea0e1c9b5440c4397b
GustavFluur <gustavfluur@hotmail.com>
2023-10-29 14:58:45 +0000

    Added some changes base.html file yet it dosen't work with its boostrap at this time + added a new app core

--- gold_scroll/settings.py
+++ gold_scroll/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +62,10 @@ ROOT_URLCONF = 'gold_scroll.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
