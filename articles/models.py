087b77de2a759e14a908414407bd8e2360bad788
GustavFluur <gustavfluur@hotmail.com>
2023-11-04 14:42:52 +0000

    Added features and vital elements for articles app

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=220)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Article (models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+    description = models.TextField()
+    price = models.FloatField()
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(upload_to='article_images', blank=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='articles', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+
--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from articles.models import Category, Article 
+
 # Create your views here.
 def index(request):
-    return render(request, 'core/index.html')
+    articles = Article.objects.filter(is_sold=False)[0:6]
+    Categories = Category.objects.all()
+
+
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'articles': articles,
+    })
