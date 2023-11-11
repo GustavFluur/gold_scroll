a77305bdf7f6aadb3646a46de77d7b9c2bfdbee6
GustavFluur <gustavfluur@hotmail.com>
2023-11-11 19:46:27 +0000

    Added features related to user orientation on the site, issues for now in terms of article detail, will be look at tomorrow

--- articles/urls.py
+++ articles/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.omnis_articles, name='articles')
+    path('', views.omnis_articles, name='articles'),
+    path('<article_id>', views.article_detail, name='article_detail'),
 ]
 
--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 # Create your views here.
@@ -7,9 +7,19 @@ from .models import Article
 def omnis_articles(request):
 
     articles = Article.objects.all()
-    print(articles)
     context = {
         'articles': articles,
     }
 
     return render(request, 'articles/articles.html', context)
+
+
+def article_detail(request, article_id):
+
+    article = get_object_or_404(Article, pk=article_id)
+
+    context = {
+        'article': article,
+    }
+
+    return render(request, 'articles/articles_detail.html', context)
