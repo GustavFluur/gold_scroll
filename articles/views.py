21990d8a17677ed6497240d73edb51b07fbcfa0d
GustavFluur <gustavfluur@hotmail.com>
2023-11-12 17:14:26 +0000

    Added code from walkthrough project to make price & rating filtering work

--- articles/views.py
+++ articles/views.py
@@ -11,30 +11,44 @@ def omnis_articles(request):
     articles = Article.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
-        print(request.GET)
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                articles = articles.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            articles = articles.order_by(sortkey)
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
-            print(categories)
             articles = articles.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-     
 
-    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "search criteria denied - please search again!")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('articles'))
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             articles = articles.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+    
     context = {
         'articles': articles,
         'search_keyword': query,
         'contemporary_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'articles/articles.html', context)
