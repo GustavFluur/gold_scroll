0dfd618b3fd5262432e0720312c8f7c679aaee6b
GustavFluur <gustavfluur@hotmail.com>
2023-11-12 15:09:07 +0000

    Added code from the walkthrough project into views.py in articles

--- articles/views.py
+++ articles/views.py
@@ -10,6 +10,13 @@ def omnis_articles(request):
 
     articles = Article.objects.all()
     query = None
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
      
 
     if request.GET:
