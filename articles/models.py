ed50bd9fd559f50578cadba5551520f480662669
GustavFluur <gustavfluur@hotmail.com>
2023-11-14 20:32:01 +0000

    Added changes into the models of article folder

--- articles/models.py
+++ articles/models.py
@@ -5,19 +5,20 @@ class Category(models.Model):
     name = models.CharField(max_length=220)
 
     class Meta:
+        ordering = ('name',)
         verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
 
 
-class Article (models.Model):
+class Article(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=300)
     description = models.TextField()
     price = models.FloatField()
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image_url = models.URLField(max_length=9999, null=True, blank=True)
+    image_url = models.URLField(null=True, blank=True)
     image = models.ImageField(upload_to='article_images', blank=True)
     is_sold = models.BooleanField(default=False)
     created_by = models.ForeignKey(User, related_name='articles', on_delete=models.CASCADE)
