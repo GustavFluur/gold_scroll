61616c59820624dab07bfa8334c82b205e736bd1
GustavFluur <gustavfluur@hotmail.com>
2023-11-19 13:59:07 +0000

    Added more features into acquisition page

---
+++ acquisition/contexts.py
@@ -0,0 +1,16 @@
+
+
+def acquisition_contents(request):
+
+    purchase_items = []
+    total = 0
+    article_count = 0
+
+
+    context = {
+        'purchase_items': purchase_items,
+        'total': total,
+        'article_count': article_count,
+    }
+
+    return context
