04dec2af3903b463e80abdd12fc972a60d68eea2
GustavFluur <gustavfluur@hotmail.com>
2023-11-19 20:04:20 +0000

    Added add functionality to acquisition inspired by walkthrough project

--- acquisition/urls.py
+++ acquisition/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.acquisition_view, name='acquisition_view')
+    path('', views.acquisition_view, name='acquisition_view'),
+    path('add/<item_id>', views.add_to_acquisition, name='add_to_acquisition'),
 ]
 
--- acquisition/views.py
+++ acquisition/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def acquisition_view(request):
 
     return render(request, 'acquisition/acquisition.html')
+
+
+
+def add_to_acquisition(request, item_id):
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    acquisition = request.session.get('acquisition', {})
+
+    if item_id in list(acquisition.keys()):
+        acquisition[item_id] += quantity
+    else:    
+        acquisition[item_id] = quantity
+    
+    request.session['acquisition'] = acquisition
+    print(request.session['acquisition'])
+    return redirect(redirect_url)
