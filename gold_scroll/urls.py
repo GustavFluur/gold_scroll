529b5bc74b82aa5f8a665ce130721d0591570a22
GustavFluur <gustavfluur@hotmail.com>
2023-11-23 13:24:26 +0000

    Added urls and now prepare templates into the checkout app

--- gold_scroll/urls.py
+++ gold_scroll/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('articles/', include('articles.urls')),
     path('acquisition/', include('acquisition.urls')),
+    path('checkout/', include('checkout.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
