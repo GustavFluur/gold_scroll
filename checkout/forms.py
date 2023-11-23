57c5227348bb0f2e430f1f68f622fdc4198103d7
GustavFluur <gustavfluur@hotmail.com>
2023-11-23 12:54:42 +0000

    Added signals, change of admin and forms inspired by boutique ado

---
+++ checkout/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Customer_Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Customer_Order
+        fields = ('first_name', 'last_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'first_name': 'Last Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'zip_code': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'county': 'County',
+        }
+
+        self.fields['last_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
