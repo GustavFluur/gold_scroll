03578f0cb7b52340f593bc0ecb0c5decf076332b
GustavFluur <gustavfluur@hotmail.com>
2023-11-24 22:01:29 +0000

    Added and influenced by walktrhough project Boutique Ado to enhance the chechout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,31 +1,108 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
+from .models import Order, OrderLineObject
+from articles.models import Article
 from acquisition.contexts import acquisition_contents
 
 import stripe 
 
 
 def checkout(request):
-    acquisition = request.session.get('acquisition', {})
-    if not acquisition:
-        messages.error(request, "There's nothing in your cart at the moment")
-        return redirect(reverse('articles'))
+    
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    
+    if request.method == 'POST':
+        acquisition = request.session.get('acquisition', {})
+
+        customer_order_data = {
+            'first_name': request.POST['first_name'],
+            'last_name': request.POST['last_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'zip_code': request.POST['zip_code'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+            'gender': request.POST['gender'],
+            'date_of_birth': request.POST['date_of_birth'],
+        }
+        order_form = OrderForm(customer_order_data)
+        if customer_order_data.is_valid():
+            order = customer_order_data.save()
+            for item_id, item_data in acquisition.items():
+                try:
+                    article = Article.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_object = OrderLineObject(
+                            order=order,
+                            article=article,
+                            quantity=item_data,
+                        )
+                        order_line_object.save()
+                except Article.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your bag wasn't found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('acquisition_view'))
+
+            request.session['save_purchase_data'] = 'save_purchase_data' in request.POST
+            return redirect(reverse('checkout_success', args=[order.distribution_number]))
+        else:
+            messages.error(request, 'Your order form did not proceed accordingly due to an error! Please check it again! ')
+    else:    
+        acquisition = request.session.get('acquisition', {})
+        if not acquisition:
+            messages.error(request, "There's nothing in your cart at the moment")
+            return redirect(reverse('articles'))form_data
+
+        contemporary_acquisition = acquisition_contents(request)
+        overall = contemporary_acquisition['summary']
+        stripe_total = round(overall * 100)
+        stripe.api_key = stripe_secret_key
+        card_objective = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
 
 
-    contemporary_acquisition = acquisition_contents(request):
-    overall = contemporary_acquisition['summary']
-    stripe_total = round(overall * 100)
+        order_form = OrderForm()
 
-    order_form = OrderForm()
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51NzOBUJzduBwqSE8C2wgQZiYAnGslRqdLY7Ab9z0ogX05NFsvY1nkXRwyIrNuLqgZqkBvGPWkHyHzooKDavt9HRy00IooGJpEL',
-        'client_secret': 'sk_test_51NzOBUJzduBwqSE8dbWCtv7jIzi3VZH2Xopw6wx3B0x0pKBZCf4SPbwlKUb37B8G1Hp3U12y50YQAeMrNyaUiJwW004bq2VexU',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': client_secret,
         
     }
 
     return render(request, template, context)
+
+
+def successful_payment(request, distribution_number):
+
+    save_purchase_data = request.session.get('save_purchase_data')
+    order = get_object_or_404(Order, distribution_number=distribution_number)
+
+    messages.success(request, f'Your order has been received! \
+        Your distribution number is {distribution_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'acquisition' in request.session:
+        del request.session['acquisition']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
+
