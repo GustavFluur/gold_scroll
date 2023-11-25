7d7829acd99d6fdc9946ce752883a48d3000ae9a
GustavFluur <gustavfluur@hotmail.com>
2023-11-25 14:32:48 +0000

    Added code from boutique ado by following the lecture

--- checkout/models.py
+++ checkout/models.py
@@ -25,6 +25,9 @@ class Customer_Order(models.Model):
     shipping_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     sum_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    original_acquisition = models.TextField(null=False, blank=False, default='')
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
 
     def produce_distribution_number(self):
         
