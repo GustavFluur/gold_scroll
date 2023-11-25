840a081421216289c411d902f7bbfc1316c6dd63
GustavFluur <gustavfluur@hotmail.com>
2023-11-25 20:01:58 +0000

    Added some new look for the forms thanks to Boutique Ado

--- checkout/forms.py
+++ checkout/forms.py
@@ -25,7 +25,7 @@ class OrderForm(forms.ModelForm):
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'Region',
         }
 
         self.fields['last_name'].widget.attrs['autofocus'] = True
