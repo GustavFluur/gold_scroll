bbe53d399c2f3894c830be00c90b27768123d6d2
GustavFluur <gustavfluur@hotmail.com>
2023-11-25 09:57:18 +0000

    Added all the code from the webhooks from walktrhough project

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('purchase_success/<distribution_number>', views.successful_payment, name='purchase_success'),
+    path('wh/', webhook, name='webhook'),
 
 ]
